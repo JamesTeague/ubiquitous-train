5a3a8e3515796436e8024948ac78bf829441b8b6
jteagueii <jtteague13@gmail.com>
2018-01-28 08:24:50 -0600

    refs #4 - [master] - This is the initial commit to the project. This creates a nodeJS app that uses expressjs that has one test route that will serve a view styled by bootstrap. This view will be changed as it was only intended to get the project in a state that it would run. Env files were added for differences as running as a developer vs production.

---
+++ app.js
@@ -0,0 +1,62 @@
+var express = require('express');
+var app = express();
+var env = process.env.NODE_ENV || 'production';
+var isDevelopmentEnv = (env === 'development');
+if (isDevelopmentEnv) {
+   require('dotenv').config({path: './config/dev.env'});
+}
+else {
+   require('dotenv').config({path: 'prod.env'});
+}
+
+// setup ===================================
+var port = process.env.PORT || 3000;
+var path = require('path');
+var bodyParser = require('body-parser');
+// app.locals.routes = require('./routes/routes');
+
+// configuration ===========================
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'pug');
+app.enable('trust proxy');
+// app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, '/node_modules/bootstrap/dist')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// routes ==================================
+require('./routes/routes')(app);
+
+// error handling ==========================
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+   var err = new Error('Not Found');
+   err.status = 404;
+   next(err);
+});
+
+// will print stacktrace
+if (isDevelopmentEnv) {
+   app.use(function(err, req, res, next) {
+      res.status(err.status || 500);
+      res.render('error', {
+         message: err.message,
+         error: err
+      });
+      // res.render('jumbotron', {title: 'Test Jumbotron'});
+   });
+}
+
+// no stacktraces leaked to user
+app.use(function(err, req, res, next) {
+   res.status(err.status || 500);
+   res.render('error', {
+      message: err.message,
+      error: {}
+   });
+});
+
+
+//launch ===================================
+app.listen(port);
+console.log("Listening on port %s", port);
