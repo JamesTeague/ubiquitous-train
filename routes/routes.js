ab7416a99c604c5f26e7ddeaa4f70a57804d3b7e
jteagueii <jtteague13@gmail.com>
2018-02-17 17:15:04 -0600

    refs #21 - [development] - adds render logic to disable log out button if page is told that the user is not logged in

--- routes/routes.js
+++ routes/routes.js
@@ -1,11 +1,11 @@
 exports.login = function(req, res) {
-   res.render("login", {title: "Retreat Sign In"});
+   res.render("login", {title: "Retreat Sign In", loggedIn: false});
 };
 
 exports.home = function(req, res) {
-   res.render("main", {title: "The Retreat"});
+   res.render("main", {title: "The Retreat", loggedIn: true});
 };
 
 exports.signUp = function(req, res) {
-   res.render('signup', {title: "The Retreat Sign Up"})
+   res.render('signup', {title: "The Retreat Sign Up", loggedIn: false});
 };
